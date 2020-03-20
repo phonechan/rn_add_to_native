51d7f3de2b30a039c91fcd62d4821cf09c4f2780
galen.jiang <galen.jiang@cardinfolink.com>
2020-03-20 15:35:03 +0800

    解决冲突

--- android/app/src/main/java/com/everonet/demo/miniprograms/api/ApiClient.java
+++ android/app/src/main/java/com/everonet/demo/miniprograms/api/ApiClient.java
@@ -20,6 +20,7 @@ import okhttp3.Response;
 import okhttp3.ResponseBody;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+import com.facebook.stetho.okhttp3.StethoInterceptor;
 
 
 /**
